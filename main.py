37241222feacd413fc44bc817adb93ce43d0f6f7
Muhammad Inam <muhammadinam247@gmail.com>
2026-09-14 13:44:29 +0500

    add new file main.py

---
+++ main.py
@@ -0,0 +1,4 @@
+first_number = int(input("Enter first number: "))
+second_number = int(input("Enter second number: "))
+sum = first_number + second_number
+print("The sum of", first_number, "and", second_number, "is:", sum)
