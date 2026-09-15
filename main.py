41a7985eeb5bd0cf85f541871f8a9ade0e92d484
Muhammad Inam <muhammadinam247@gmail.com>
2026-09-15 13:50:10 +0500

    delete main.py and renamed jokes.py to programmer_jokes.py

--- main.py
+++
@@ -1,4 +0,0 @@
-first_number = int(input("Enter first number: "))
-second_number = int(input("Enter second number: "))
-sum = first_number + second_number
-print("The sum of", first_number, "and", second_number, "is:", sum)
